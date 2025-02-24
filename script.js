71bb4ce6f6070ecb810a10cfc76bf4354d757938
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-24 22:32:26 +0000

    display copied message

--- script.js
+++ script.js
@@ -1,114 +1,42 @@
-// Variable
+// Variables
 let generate = document.querySelector(".jsGenerate");
 let passwordInput = document.querySelector(".jsPasswordInput");
 let copy = document.querySelector(".jsCopy");
 let copied = document.querySelector(".jsCopied");
 
-// Password characters array
-let characters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "-",
-  "_",
-  "/",
-  "(",
-  ")",
-  "]",
-  "[",
-  "#",
-  "%",
-  ".",
-  ",",
-  "'",
-  "@",
-];
+// Password length
+let length = 12;
 
-// Generates random number the length or characters array and returns the index value of characters using that random number
-let randomCharacter = () => {
-  let randomNum = Math.floor(Math.random() * characters.length);
-  let randomChar = characters[randomNum];
-  return randomChar;
-};
+// Characters
+let allCharacters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_+~|}{[]<>/-=";
 
-// Input gets a 12 digit random combination of characters
+// Create a random 12 digit password from allCharacters
+function createPassword() {
+  let password = "";
+  while (length > password.length) {
+    password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+  passwordInput.value = password;
+}
+
+// Run createPassword on generate button click
 generate.addEventListener("click", () => {
-  passwordInput.value = `${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}${randomCharacter()}`;
+  createPassword();
 });
 
 // Function that allows the input to be copied to clipboard
-function copyText() {
-  let range = document.createRange();
-  range.selectNode(document.querySelector(".jsPasswordInput"));
-  window.getSelection().removeAllRanges(); // clear current selection
-  window.getSelection().addRange(range); // to select text
+function copyPassword() {
+  passwordInput.select();
   document.execCommand("copy");
-  window.getSelection().removeAllRanges(); // to deselect
+  passwordInput.setSelectionRange(0, 0);
+  passwordInput.blur();
 }
 
 // When clicking copy icon, calls copyText and displays a message that times out after 1 second or restarts if clicked again
 let isCopied = false;
 copy.addEventListener("click", () => {
-  copyText();
+  copyPassword();
   copied.style.display = "inline";
   if (!isCopied) {
     isCopied = true;
